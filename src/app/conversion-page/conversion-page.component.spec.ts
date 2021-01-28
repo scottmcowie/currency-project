e6bdf321bb571acf2d8371742398f65bb00374a1
cowiesc <scott_cowie@slc.co.uk>
2021-01-28 20:54:20 +0000

    gfhh

--- src/app/conversion-page/conversion-page.component.spec.ts
+++ src/app/conversion-page/conversion-page.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ConversionPageComponent } from './conversion-page.component';
 
@@ -6,7 +6,7 @@ describe('ConversionPageComponent', () => {
   let component: ConversionPageComponent;
   let fixture: ComponentFixture<ConversionPageComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ConversionPageComponent ]
     })
